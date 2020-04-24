9880a3610458375497cd436575ee67bb281e6f6b
soumik-mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2020-04-24 21:33:12 +0530

    feat: Add github event type as file metadata

--- src/resources/Event.js
+++ src/resources/Event.js
@@ -5,6 +5,7 @@ import util from 'util';
 
 
 const GH_DELIVERY_HDR = "x-github-delivery";
+const GH_EVENT_HDR = "x-github-event";
 const _pipeline = util.promisify(pipeline);
 
 export const Event = {
@@ -25,22 +26,20 @@ export const Event = {
         const ghDeliveryId = request.headers[GH_DELIVERY_HDR];
         const newFileName = `src-events-raw/gh_event_${ghDeliveryId}.json`;
         const newFile = srcBucket.file(newFileName);
+        const newFileMetadata = {
+          contentType: 'application/json',
+          metadata: {
+            'X-GitHub-Event': request.headers[GH_EVENT_HDR]
+          }
+        };
+
+        await newFile.setMetadata(newFileMetadata);
 
         const payloadAsStream = Readable.from(JSON.stringify(request.payload));
         const newFileAsStream = newFile.createWriteStream();
 
         await _pipeline(payloadAsStream, newFileAsStream);
-        /*payloadAsStream.pipe(newFileAsStream)
-          .on("error", function (err) {
-            console.error(err);
-            //return h.response("Internal error").code(500);
-            newFileAsStream.end();
 
-          })
-          .on("finish", function () {
-            console.log("Finished writing event file");
-          });
-          */
           return h.response("created").type("text/plain").code(201);
       } catch (err) {
         console.error(err);
